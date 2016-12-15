7257252b20715c96ff46d7e626b30f34df531e01
Morgan Packard <mpackard@wayfair.com>
2016-12-15 13:35:06 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let img = new Image();
+  img.width = 40;
+  img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa,_by_Leonardo_da_Vinci,_from_C2RMF_retouched.jpg/402px-Mona_Lisa,_by_Leonardo_da_Vinci,_from_C2RMF_retouched.jpg";
+  document.querySelector("body").appendChild(img);
+});
